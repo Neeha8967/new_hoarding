438ad566b89e14f07b9c539fef3f452307b8f3b8
Neeha Ghosh <85061556+Neeha8967@users.noreply.github.com>
2023-09-09 16:09:55 +0530

    script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (username === "admin" && password === "admin12@") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
